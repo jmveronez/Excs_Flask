b1f8f41627c200b9685766919c1ef249234b1b09
jmveronez <jmveronez234@gmail.com>
2021-07-02 23:16:03 -0300

    Resolução dos Exercícios

--- Projeto_1/app.py
+++ Projeto_1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import random
 
@@ -40,7 +40,14 @@ class filme(db.Model):
         filmes4 = filmes.append(filme.query.order_by(filme.nome.desc()).limit(3).all())
         filmes_aleatorios = random.choice(filmes)
         return filmes_aleatorios
+    
+    @staticmethod
+    def read_id(registro_id):
+        return filme.query.get(registro_id)
 
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
 @app.route("/")
 def index():
@@ -54,13 +61,23 @@ def read_all():
     print(registros)
     return render_template("read_all.html", registros=registros)
 
-@app.route("/create")
+@app.route("/create", methods=('GET', 'POST'))
 def create():
-    return render_template("create.html")
+    novo_id = None
+    if request.method == 'POST':
+        form = request.form
+        
+        registro = filme(form['nome'], form['imagem_url'])
+        registro.save()
+
+        novo_id = registro.id
+
+    return render_template("create.html", novo_id = novo_id)
 
 @app.route("/read/<id_registro>")
 def read_id(id_registro):
     registro = filme.read_id(id_registro)
+    return render_template("read_id.html", registro=registro)
 
 if (__name__ == '__main__'):
     app.run(debug=True)
